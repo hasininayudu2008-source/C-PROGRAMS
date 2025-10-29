1aa49e7d1ca2e043b2f7e1669ef2a3ec5f00019b
hasininayudu2008-source <hasininayudu2008@gmail.com>
2025-10-29 14:53:11 +0530

    Add files via upload

---
+++ calculator using switch case.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	char op;
+	double first,second;
+	printf("Enter an operator(+,-,*,/):");
+	scanf("%c",&op);
+	printf("Enter two operands:");
+	scanf("%lf %lf",&first,&second);
+	switch(op)
+	{
+		case'+':
+			printf("%.lf+%.lf=%.lf",first,second,first+second);
+			break;
+		case'-':
+			printf("%.lf-%.lf=%.lf",first,second,first-second);
+			break;
+		case'*':
+			printf("%.lf*%.lf=%.lf",first,second,first*second);
+			break;
+		case'/':
+			printf("%.lf/%.lf=%.lf",first,second,first/second);
+			break;
+			//operator does not match for any case
+			default:
+				printf("ERROR!Operator is not correct");
+			
+			
+				
+	}
+	return 0;
+}
